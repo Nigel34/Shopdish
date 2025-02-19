9adbdc64bc2c252f26b6e0d4baabc42a2d767af4
Nigel34 <ichenihinigel@gmail.com>
2025-02-19 13:26:26 +0300

    third commit

---
+++ Shopapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from Shopapp import views
+
+urlpatterns = [
+      path('admin/', admin.site.urls),
+      path('', views.home, name='home'),
+      path('gallery/', views.images, name='gallery'),
+      path('about/', views.about , name='about'),
+]
